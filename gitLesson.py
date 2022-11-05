faba5c53c72109a3623e649ca11d4af26151d200
Satoshi-Ichikawa-Agri <stayhungry.stayfoolish.satoshi@gmail.com>
2022-11-05 17:19:34 +0900

    add gitLessonFile

---
+++ gitLesson.py
@@ -0,0 +1,9 @@
+print("What your name?")
+your_name = input("<< ")
+
+print(your_name)
+
+print("How old are you?")
+your_age = input("<< ")
+
+print("あなたの名前は" + your_name, "あなたの年齢は" + your_age + "歳です。")
